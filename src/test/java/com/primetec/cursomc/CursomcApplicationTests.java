48bc8b95dae511e206e1d5dcf65f698cd6a54e8c
Gabriel Novais <gsnbil@gmail.com>
2021-09-05 21:51:09 -0300

    Projeto Criado

---
+++ src/test/java/com/primetec/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primetec.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
