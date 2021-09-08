3e4a9d57716f30a39def4b9f8c098a1ac55ff006
Gabriel Novais <gsnbil@gmail.com>
2021-09-08 12:55:01 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/primetec/cursomc/services/CategoriaService.java
+++ src/main/java/com/primetec/cursomc/services/CategoriaService.java
@@ -1,10 +1,12 @@
 package com.primetec.cursomc.services;
 
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.primetec.cursomc.domain.Categoria;
 import com.primetec.cursomc.repositories.CategoriaRepository;
+import com.primetec.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,7 +16,6 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-
 }
